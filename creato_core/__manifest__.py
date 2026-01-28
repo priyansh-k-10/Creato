3f787ee2f34d802b3d819bae9f72067d9d259cf5
unknown <pk@4fo.de>
2026-01-28 12:17:13 +0530

    Removed py file

--- creato_core/__manifest__.py
+++ creato_core/__manifest__.py
@@ -24,7 +24,7 @@ by other Creato modules such as website, operations, and integrations.
     'data': [
         'security/ir.model.access.csv',
         'views/creato_base_views.xml',
-        'models/models.py',
+        # 'models/models.py',
     ],
 
     'installable': True,
