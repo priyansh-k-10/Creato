22bd0a00998c25ecf6b19d6259d04ba11fc4e646
unknown <pk@4fo.de>
2026-02-05 15:47:51 +0530

    File data changes

--- addons/creato/__manifest__.py
+++ addons/creato/__manifest__.py
@@ -5,16 +5,13 @@
     'summary': 'Core models and shared backend logic for Creato',
     'description': """
 Creato Core
-===========
-
 This module provides the foundational backend models, views,
 and shared logic for the Creato platform.
 
 It is designed as a technical base module that can be extended
 by other Creato modules such as website, operations, and integrations.
 """,
-    'author': 'Priyansh Khatri',
-    'license': 'LGPL-3',
+
     'data': [
         'security/ir.model.access.csv',
         'views/menus.xml',
