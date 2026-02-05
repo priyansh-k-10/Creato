3305b78a24a416e970c9ccb875a26602b7ed70d1
unknown <pk@4fo.de>
2026-02-05 18:18:13 +0530

    influencer file added in model

--- addons/creato/__manifest__.py
+++ addons/creato/__manifest__.py
@@ -2,20 +2,19 @@
     'name': 'Creato',
     'version': '1.0',
     'depends': ['base'],
-    'summary': 'Core models and shared backend logic for Creato',
-    'description': """
-Creato Core
-This module provides the foundational backend models, views,
-and shared logic for the Creato platform.
-
-It is designed as a technical base module that can be extended
-by other Creato modules such as website, operations, and integrations.
-""",
+    'author': 'Priyansh Khatri',
+    'license': 'LGPL-3',
 
     'data': [
         'security/ir.model.access.csv',
+
+        # 'views/brand_views.xml',
+        # 'views/influencer_views.xml',
+        # 'views/collaboration_views.xml',
+
         'views/menus.xml',
     ],
+
     'application': True,
     'installable': True,
 }
---
+++ addons/creato/models/influencer.py
@@ -0,0 +1,11 @@
+from odoo import models, fields
+
+
+class CreatoInfluencer(models.Model):
+    _name = 'creato.influencer'
+    _description = 'Influencer'
+
+    name = fields.Char(string="Name", required=True)
+    email = fields.Char(string="Email")
+    phone = fields.Char(string="Phone")
+    active = fields.Boolean(string="Active", default=True)
