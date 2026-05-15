077040ea437f75251f92dbbc253c59f80fc43508
unknown <pk@4fo.de>
2026-05-15 15:00:20 +0530

    change in Model/influencer

--- addons/creato/models/influencer.py
+++ addons/creato/models/influencer.py
@@ -1,5 +1,6 @@
 from odoo import models, fields
 
+
 class CreatoInfluencer(models.Model):
     _name = 'creato.influencer'
     _description = 'Influencer'
@@ -7,5 +8,11 @@ class CreatoInfluencer(models.Model):
     name = fields.Char(string="Name", required=True)
     email = fields.Char(string="Email")
     insta_followers = fields.Char(string="Instagram Followers")
-    insta_username = fields.Char(string="Intagram Username")
+    insta_username = fields.Char(string="Instagram Username")
     active = fields.Boolean(string="Active", default=True)
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    customer_code = fields.Char(string="Customer Code")
