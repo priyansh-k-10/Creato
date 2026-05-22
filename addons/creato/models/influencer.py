4b973091edd74b64d99ea0afd0c8cb47ababbed3
unknown <pk@4fo.de>
2026-05-22 16:38:37 +0530

    influcers redirection

--- addons/creato/models/influencer.py
+++ addons/creato/models/influencer.py
@@ -1,15 +1,26 @@
-from odoo import models, fields
 class CreatoInfluencer(models.Model):
     _name = 'creato.influencer'
     _description = 'Influencer'
 
     name = fields.Char(string="Name", required=True)
     email = fields.Char(string="Email")
-    insta_followers = fields.Char(string="instagram Followers")
-    insta_username = fields.Char(string="instagram Username")
+    insta_followers = fields.Char(string="Instagram Followers")
+    insta_username = fields.Char(string="Instagram Username")
     active = fields.Boolean(string="Active", default=True)
+
+    partner_id = fields.Many2one(
+        'res.partner',
+        string="Contact"
+    )
+
+    user_id = fields.Many2one(
+        'res.users',
+        string="User"
+    )
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
-    customer_code = fields.Char(string="Customer Code")
+    customer_code = fields.Char(
+        string="Customer Code"
+    )
